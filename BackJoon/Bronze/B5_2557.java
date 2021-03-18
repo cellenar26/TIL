1186107f38ec1c35f3e177d3821676d4634f60eb
cellenar26 <cellenar26@gmail.com>
2021-03-18 21:20:22 +0900

    b2_2557

---
+++ BackJoon/Bronze/B5_2557.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import static java.lang.Math.pow;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // b5_2557
+        System.out.println("Hello World!");
+    }
+}
+
+//class Solution {
+//    public int solution(int s) {
+//        String[] temp = Integer.toString(s).split("");
+//        int result = 0;
+//
+//        for (int i=0 ; i < temp.length; i++) {
+//            result = result + Integer.parseInt(temp[i].toString());
+//        }
+//
+//        return result;
+//    }
+//}
